961c056af995cb8e821e16bf122b42ce29261b13
quietcoolwu <quietcool@163.com>
2015-10-16 13:32:08 +0800

    hello

--- 049 Anagrams.py
+++ 049 Anagrams.py
@@ -4,6 +4,8 @@ Given an array of strings, return all groups of strings that are anagrams.
 Note: All inputs will be in lower-case.
 """
 __author__ = 'Danyang'
+
+
 class Solution:
     def anagrams_complicated(self, strs):
         """
@@ -13,12 +15,11 @@ class Solution:
         """
         temp = list(strs)
         for ind, string in enumerate(temp):
-            if string and string!="":  # avoid case of empty string
+            if string and string != "":  # avoid case of empty string
                 string = [char for char in string]
                 string.sort()
                 string = "".join(string)
                 temp[ind] = string
-
 
         hash_map = {}
         for ind, string in enumerate(temp):
@@ -28,7 +29,7 @@ class Solution:
 
         result = []
         for val in hash_map.values():
-            if len(val)>1:
+            if len(val) > 1:
                 # result.append([strs[i] for i in val])
                 result += [strs[i] for i in val]
         return result
@@ -46,16 +47,17 @@ class Solution:
                 hash_map[string] = [ind]
             else:
                 hash_map[string].append(ind)
-            # indexes = hash_map.get(string, [])
-            # indexes.append(ind)  # side-effect
-            # hash_map[string] = indexes  # reassign
+                # indexes = hash_map.get(string, [])
+                # indexes.append(ind)  # side-effect
+                # hash_map[string] = indexes  # reassign
 
         result = []
         for val in hash_map.values():
-            if len(val)>1:
+            if len(val) > 1:
                 # result.append([strs[i] for i in val])
                 result += [strs[i] for i in val]
         return result
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     Solution().anagrams(["", ""])
